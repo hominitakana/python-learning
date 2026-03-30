e34c8cfcda5d16a544d6dfcc4f0622d846cff246
hominitakana <afs.yj16@icloud.com>
2026-03-30 17:08:52 +0900

    add df_departments_amount_all

--- analysis.py
+++ analysis.py
@@ -15,7 +15,7 @@ conn = psycopg2.connect(
 #cur（カーソル）はその接続を使って実際にSQLを実行する窓口
 cur = conn.cursor()
 
-
+# SQLを取得、データフレームワークに変換するまでの関数
 def get_dataframe(cur,sql_message):
     # SQLを実行
     cur.execute(sql_message)
@@ -32,13 +32,20 @@ def get_dataframe(cur,sql_message):
     return df
 
 
+
+# employeesのdfを取得
 df_employees = get_dataframe(cur,
                 """
                 SELECT
-                    *
+                    id as employee_id,
+                    name as employee_name,
+                    department_id
+                    --position,
+                    --hire_date
                 FROM employees;
                 """)
 
+# departmentsのdfを取得
 df_departments = get_dataframe(cur,
                 """
                 SELECT
@@ -48,13 +55,54 @@ df_departments = get_dataframe(cur,
                 FROM departments;
                 """)
 
+# salariesのdfを取得
+df_salaries  = get_dataframe(cur,
+                """
+                SELECT
+                    id as salaries_id,
+                    employee_id,
+                    amount
+                FROM salaries ;
+                """)
 
-df_emp_dept  = pd.merge(df_employees, df_departments, on="department_id", how="left")
-department_count = df_emp_dept.groupby("department_name")["id"].count().reset_index()
+
+# 部署ごとに人数を多い順に並べる
+df_employees_departments  = pd.merge(df_employees, df_departments, on="department_id", how="left")
+department_count = df_employees_departments.groupby("department_name")["department_id"].count().reset_index()
 department_count.columns = ["部署名", "人数"]
 desc_department_count = department_count.sort_values("人数", ascending=False)
 
-print(desc_department_count)
+# 部署ごとの平均給与が高い順に並べる
+df_employees_salaries_departments  = pd.merge(df_employees_departments, df_salaries, on="employee_id", how="inner")
+department_avg_amount = df_employees_salaries_departments.groupby("department_name")["amount"].mean().reset_index()
+
+department_avg_amount.columns = ["部署名", "平均給与"]
+desc_department_avg_amount = department_avg_amount.sort_values("平均給与", ascending=False)
+
+
+#部署名 / 人数 / 平均給与 / 最高給与 / 最低給与 の5カラム
+department_max_amount = df_employees_salaries_departments.groupby("department_name")["amount"].max().reset_index()
+department_min_amount = df_employees_salaries_departments.groupby("department_name")["amount"].min().reset_index()
+
+department_max_amount.columns = ["部署名", "最高給与"]
+department_min_amount.columns = ["部署名", "最低給与"]
+
+df_departments_amount_all = (department_count
+                                .merge(department_avg_amount, on="部署名", how="inner")
+                                .merge(department_max_amount,  on="部署名", how="inner")
+                                .merge(department_min_amount,  on="部署名", how="inner")
+                                )
+
+df_departments_amount_all['平均給与'] = df_departments_amount_all['平均給与'].astype('int')
+df_departments_amount_all['最高給与'] = df_departments_amount_all['最高給与'].astype('int')
+df_departments_amount_all['最低給与'] = df_departments_amount_all['最低給与'].astype('int')
+
+df_departments_amount_all = df_departments_amount_all.sort_values("平均給与", ascending=False)
+
+
+# print(desc_department_count)
+# print(desc_department_avg_amount)
+print(df_departments_amount_all)
 
 
 # 接続を閉じる
