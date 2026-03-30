8698ce27ea071f5787aeb8bdfa23e36eca818dc8
hominitakana <afs.yj16@icloud.com>
2026-03-30 16:15:46 +0900

    add desc_department_count

--- analysis.py
+++ analysis.py
@@ -15,20 +15,6 @@ conn = psycopg2.connect(
 #cur（カーソル）はその接続を使って実際にSQLを実行する窓口
 cur = conn.cursor()
 
-# SQLを実行
-cur.execute("""
-            SELECT
-                *
-            FROM employees;
-            """)
-
-#結果を全件取得
-rows = cur.fetchall()
-
-# cur.description : カラム名の情報を取得
-columns = [desc[0] for desc in cur.description]
-
-
 
 def get_dataframe(cur,sql_message):
     # SQLを実行
@@ -63,8 +49,12 @@ df_departments = get_dataframe(cur,
                 """)
 
 
-result = pd.merge(df_employees, df_departments, on="department_id", how="left")
-print(result)
+df_emp_dept  = pd.merge(df_employees, df_departments, on="department_id", how="left")
+department_count = df_emp_dept.groupby("department_name")["id"].count().reset_index()
+department_count.columns = ["部署名", "人数"]
+desc_department_count = department_count.sort_values("人数", ascending=False)
+
+print(desc_department_count)
 
 
 # 接続を閉じる
