4068f62a45f1f9ed83fbf1d5805673d61e0dbd17
hominitakana <afs.yj16@icloud.com>
2026-03-31 16:45:08 +0900

    add streamlit

--- test_api.py
+++ test_api.py
@@ -1,19 +1,22 @@
 import requests
 
 def main():
-    url = "http://127.0.0.1:8000/item/"
+    url = "http://127.0.0.1:8000/"
     body = {
-        "ShopInfo": {
+        "shop_info": {
             "name": "B shop",
             "location": "Shizuoka"
         },
-        "Item": {
+        "items": [
+            {
             "name": "T-shirt",
             "description": "This in normal T-shirt",
             "price": "500",
             "tax": "1.1"
-        },
+            }
+        ]
     }
+    
     res = requests.post(url, json = body)
     print(res.json())
 
